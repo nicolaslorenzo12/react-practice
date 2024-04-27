e5623a236a9890411b8e520cfd2d567d13110e7e
niselo <nicolas.lorenzo@student.kdg.be>
2024-04-27 18:26:19 +0200

    new supermarket null value still not totally handled

--- src/index.js
+++ src/index.js
@@ -3,27 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Product from "./pages/Product";
 import { Supermarket } from "./pages/Supermarket";
-import { SupermarketTable } from "./pages/Supermarket";
+// import { SupermarketTable } from "./pages/Supermarket";
 import Contact from "./pages/Contact";
 import ProductsOfSupermarket from "./pages/ProductsOfSupermarket";
-import AddSupermarket from "./pages/AddSupermarket";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* <Route index element={<Supermarket />} /> */}
-          <Route index element={
+          <Route index element={<Supermarket />} />
+          {/* <Route index element={
             <>
               <Supermarket />
               <SupermarketTable />
             </>
-          } />
+          } /> */}
           <Route path="products" element={<Product/>} />
           <Route path="contact" element={<Contact/>} />
           <Route path="productsofasupermarket/:supermarketId" element={<ProductsOfSupermarket/>} />
-          <Route path="addsupermarket" element={<AddSupermarket/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Supermarket.js
+++ src/pages/Supermarket.js
@@ -39,7 +39,66 @@
 // export default Supermarket;
 
 
-import React from "react";
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import '../style/generalstyle.css';
+
+// const Supermarket = () => {
+//   return (
+//     <div>
+//       <h1 className="centered-header">Supermarkets</h1>
+//       <Link to="/addsupermarket" className="action-button">
+//       </Link>
+//     </div>    
+//   );
+// };
+
+// // Define the SupermarketTable component
+// const SupermarketTable = () => {
+//   return (
+//     <div>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Supermarket Name</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>
+//               <Link to={`/productsofasupermarket/${1}`}>NicoSupermarket</Link>
+//             </td>
+//           </tr>
+//           <tr>
+//             <td>
+//               <Link to={`/productsofasupermarket/${2}`}>MartijnSupermarket</Link>
+//             </td>
+//           </tr>
+//           <tr>
+//             <td>
+//               <Link to={`/productsofasupermarket/${3}`}>GabySupermarket</Link>
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+
+//       <Link className="add-or-delete-button">
+//           <button className="action-button">Add Supermarket</button>
+//       </Link>
+//     </div>
+    
+//   );
+// };
+
+// export { Supermarket, SupermarketTable }
+
+
+
+
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import '../style/generalstyle.css';
 
@@ -47,14 +106,78 @@ const Supermarket = () => {
   return (
     <div>
       <h1 className="centered-header">Supermarkets</h1>
-      <Link to="/addsupermarket" className="action-button">
-      </Link>
+      <SupermarketManagement />
     </div>    
   );
 };
 
-// Define the SupermarketTable component
-const SupermarketTable = () => {
+const SupermarketManagement = () => {
+  const [showForm, setShowForm] = useState(false);
+  const [supermarkets, setSupermarkets] = useState([
+    { id: 1, name: "NicoSupermarket" },
+    { id: 2, name: "MartijnSupermarket" },
+    { id: 3, name: "GabySupermarket" }
+  ]);
+  const [newSupermarketName, setNewSupermarketName] = useState("");
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+    setNewSupermarketName(""); 
+    setShowErrorMessage(false); // Reset error message state
+  };
+
+  const handleInputChange = (e) => {
+    setNewSupermarketName(e.target.value);
+  };
+
+  const handleAddSupermarket = () => {
+    if (newSupermarketName !== "") {
+      const newSupermarket = {
+        id: supermarkets.length + 1,
+        name: newSupermarketName
+      };
+      setSupermarkets([...supermarkets, newSupermarket]);
+      setNewSupermarketName(""); // Erase the previous name you added in the input
+      setShowForm(false); // Hide the form after adding the new supermarket
+    }
+    else{
+      setShowErrorMessage(true);
+    }
+  
+  };
+
+  return (
+    <div>
+      {showForm ? (
+        <form>
+          <input
+            type="text"
+            value={newSupermarketName}
+            onChange={handleInputChange}
+          />
+          <div className="state-button">
+            <button onClick={handleAddSupermarket}>Add Supermarket</button>
+          </div>
+          <div className="state-button">
+            <button onClick={toggleForm}>Cancel</button>
+            {showErrorMessage && <p id="new-supermarket-name-null">The name of the supermarket can not be null</p>}
+            {/* <p id="new-supermarket-name-null">The name of the supermarket can not be null</p> */}
+          </div>
+        </form>
+      ) : (
+        <div>
+          <SupermarketTable supermarkets={supermarkets} />
+          <div className="state-button">
+            <button onClick={toggleForm}>Add Supermarket</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const SupermarketTable = ({ supermarkets }) => {
   return (
     <div>
       <table>
@@ -64,31 +187,20 @@ const SupermarketTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <Link to={`/productsofasupermarket/${1}`}>NicoSupermarket</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to={`/productsofasupermarket/${2}`}>MartijnSupermarket</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to={`/productsofasupermarket/${3}`}>GabySupermarket</Link>
-            </td>
-          </tr>
+          {supermarkets.map(supermarket => (
+            <tr key={supermarket.id}>
+              <td>
+                <Link to={`/productsofasupermarket/${supermarket.id}`}>
+                  {supermarket.name}
+                </Link>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-
-      <Link to="/addsupermarket" className="add-or-delete-button">
-          <button className="action-button">Add Supermarket</button>
-      </Link>
     </div>
-    
   );
 };
 
-export { Supermarket, SupermarketTable }; // Exporting both components
+export { Supermarket };
 
