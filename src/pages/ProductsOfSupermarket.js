2b912dd4d8023a3f80c89ffba10bfacacccc7868
niselo <nicolas.lorenzo@student.kdg.be>
2024-05-08 12:35:24 +0200

    code improved

--- src/pages/ProductsOfSupermarket.js
+++ src/pages/ProductsOfSupermarket.js
@@ -26,45 +26,36 @@ const ProductsOfASupermarket = () =>{
     return [productList1, productList2, productList3]
 }
 
+function getSelectedSupermarket(supermarketId, productLists) {
+    let selectedProductList;
+    let supermarketName;
+    let error = null;
+
+    if (supermarketId === "1") {
+        selectedProductList = productLists[0];
+        supermarketName = "NicoSupermarket";
+    } else if (supermarketId === "2") {
+        selectedProductList = productLists[1];
+        supermarketName = "MartijnSupermarket";
+    } else if (supermarketId === "3") {
+        selectedProductList = productLists[2];
+        supermarketName = "GabySupermarket";
+    } else {
+        error = <h2>Invalid supermarketId</h2>;
+    }
+
+    return { selectedProductList, supermarketName, error };
+}
+
 
 const TableOfProductsOfASupermarket = () => {
     let { supermarketId } = useParams();
     const productLists = ProductsOfASupermarket()
+    const { selectedProductList, supermarketName, error } = getSelectedSupermarket(supermarketId, productLists);
 
-    
-    // const productList1 = [
-    //     { productName: "Coca-cola", price: "€1.00" },
-    //     { productName: "Fanta", price: "€1.10" },
-    //     { productName: "Ice tea", price: "€1.20" }
-    // ];
-
-    // const productList2 = [
-    //     { productName: "Coca-cola", price: "€1.30" },
-    //     { productName: "Fanta", price: "€1.40" },
-    //     { productName: "Ice tea", price: "€1.50" }
-    // ];
-
-    // const productList3 = [
-    //     { productName: "Coca-cola", price: "€1.60" },
-    //     { productName: "Fanta", price: "€1.70" },
-    //     { productName: "Ice tea", price: "€1.80" }
-    // ];
-
-    let selectedProductList;
-    let supermarketName;
-    if (supermarketId === "1") {
-        selectedProductList = productLists[0];
-        supermarketName = "NicoSupermarket"
-    } else if (supermarketId === "2") {
-        selectedProductList = productLists[1];
-        supermarketName = "MartijnSupermarket"
-    } else if (supermarketId === "3") {
-        selectedProductList = productLists[2];
-        supermarketName = "GabySupermarket"
-    } else {
-        return <h2>Invalid supermarketId</h2>;
+    if (error) {
+        return error;
     }
-
 
     return (
         <>
